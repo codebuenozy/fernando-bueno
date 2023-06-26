1e9c73ac236ad4dd8636e26317c01a7feced82c5
Fernando Bueno <nando_silverz@hotmail.com>
2023-06-25 21:13:02 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/Sidebar";
+
+import "../src/css/app.css";
+
+function App() {
+  
+  return (
+      <div id="portfolio">
+        <h1>Fernando Bueno</h1>
+        <Sidebar />
+        <MainContent />
+
+      </div>
+  )
+}
+
+export default App
